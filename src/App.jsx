e2903f43468aea142634d7d91588a4a4562831bc
OtnielR <endidebora@gmail.com>
2025-10-25 01:20:48 +0700

    Fix Bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { HashRouter, Routes, Route } from "react-router-dom"
 import './App.css'
 import Home from "./pages/home"
 import BlogDetails from "./pages/blog-details"
+import ScrollToTop from "./components/extra/scroll-to-top"
 
 function App() {
 
   return (
     <HashRouter>
+      <ScrollToTop />
       <Routes >
         <Route path='/' element={<Home />}></Route>
         <Route path='/blogs/:slug' element={<BlogDetails />}></Route>
