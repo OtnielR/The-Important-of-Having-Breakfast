535e97277c81fda3c35be81e80304b4e8f39ebec
OtnielR <endidebora@gmail.com>
2025-10-29 07:44:03 +0700

    Fix Nav Font

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -10,7 +10,7 @@ export default function Blog({ blog }) {
                     className=" w-full h-full object-cover transition-transform duration-500 hover:scale-100"
                 />
             </div>
-            <Link to={`/blogs/${blog.slug}`} className="mt-2 text-2xl tracking-widest font-semibold text-[#002922]">{blog.title}</Link>
+            <Link to={`/blogs/${blog.slug}`} className="mt-2 text-2xl  font-semibold text-[#002922]">{blog.title}</Link>
             <p className="truncate-2-lines md:truncate-3-lines">{blog.content}</p>
         </div >
     );
